e0d69e46915a59e19fb8535651a518ca6c1c8b1f
Soham <jambhwadekarsoham@gmail.com>
2025-05-05 21:41:11 +0530

    added stakingInterface

--- components/StakingInterface.jsx
+++ components/StakingInterface.jsx
@@ -1,3 +1,22 @@
+"use client"
 import { useState } from "react";
 import web3 from "../utils/web3";
 import stakingContract from "../utils/contract";
+
+export default function StakingInterface() {
+    const[amount , setAmount] = useState("");
+    const[account , setAccount] = useState("");
+
+    const conectWallet = async () => {
+        const account = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccount(account[0]);
+    }
+
+    return(
+        <div>
+            <h1>Eth Staking DApp</h1>
+            <button onClick={conectWallet}> Connect Wallet</button>
+            <p>Connected : {account}</p>
+        </div>
+    )
+}
