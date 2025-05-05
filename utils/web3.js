4b2251eefc7acdd144d50aeeb6a74bb31e70613c
Soham <jambhwadekarsoham@gmail.com>
2025-05-05 21:41:33 +0530

    update

--- utils/web3.js
+++ utils/web3.js
@@ -4,9 +4,6 @@ let web3;
 
 if(typeof window !== "undefined" && typeof window.ethereum !== "undefined"){
     web3 = new Web3(window.ethereum);
-} else{
-    const provider = new Web3.HttpProvider("https://eth-sepolia.g.alchemy.com/v2/LxIxMyBFQSm7gGFChwLGjKyJVkkQ6LMJ");
-    web3 = new Web3(provider);
 }
 
 export default web3;
